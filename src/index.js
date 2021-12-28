7551dd0f67c66d60225bfd456cfee178ab2df477
Antonio Pierro <antonio.pierro@gmail.com>
2021-12-28 23:50:13 +0100

    feat: add express

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
 import https from 'https';
+import express from 'express';
 import fs from 'fs';
 import dotenv from 'dotenv';
 import { handler } from './handler.js';
 
 dotenv.config();
-const { PORT, KEY, CERT, CA } = process.env;
 
-var options = {
+const { PORT, KEY, CERT, CA } = process.env;
+const app = express();
+const getOptions = () => ({
     key: fs.readFileSync(KEY),
     cert: fs.readFileSync(CERT),
     ca: fs.readFileSync(CA),
-};
-var server = https.createServer(options, handler);
+});
+const server = https.createServer(getOptions(), app);
+
+app.get('/', handler);
 server.listen(PORT, () => console.log(`server listing on ${PORT}`));
